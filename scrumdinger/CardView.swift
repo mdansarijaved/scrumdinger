9704ca18b332fb14ab4e827540d045213a5829ed
Javed Ansari <javedans2003@gmail.com>
2023-11-11 01:50:28 +0530

    card view

--- scrumdinger/CardView.swift
+++ scrumdinger/CardView.swift
@@ -7,22 +7,31 @@
 
 import SwiftUI
 
-struct CardView : View{
+
+struct CardView: View {
     let scrum: DailyScrum
-    var body: some View{
-        VStack (alignment: .leading) {
+    var body: some View {
+        VStack(alignment: .leading) {
             Text(scrum.title)
                 .font(.headline)
-            HStack{
-                Label("\(scrum.attendees.count)",systemImage: "person.3")
+            Spacer()
+            HStack {
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                    .padding(.trailing, 20)
             }
+            .font(.caption)
         }
+        .padding()       
     }
+    
 }
 
-struct CardView_Previews: PreviewProvider{
+
+struct CardView_Previews: PreviewProvider {
     static var scrum = DailyScrum.sampleData[0]
-    static var previews: some View{
+    static var previews: some View {
         CardView(scrum: scrum)
             .background(scrum.theme.mainColor)
             .previewLayout(.fixed(width: 400, height: 60))
